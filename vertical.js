ab65fecb45bdf17d2e01d2970c7abdf8c4904b1d
Denys Mishunov <denys.mishunov@gmail.com>
2012-02-13 21:48:12 +0100

    JS-driven vertical slider. Not continuous yet

---
+++ vertical.js
@@ -0,0 +1,25 @@
+var cheight=430,
+    numberOfItems=4,
+    visibleItemDuration=4000,
+    transitionDuration=1000,
+    i, shift;
+
+(function ($) {
+    $.fn.animateCarousel = function() {
+        return this.each(function () {
+            for (i = 1; i < numberOfItems; i += 1) {
+                shift = -i * cheight;
+                $(this).delay(visibleItemDuration).animate({
+                    marginTop: shift
+                }, transitionDuration);
+            }
+            $(this).delay(visibleItemDuration).animate({
+                marginTop: 0
+            }, transitionDuration);
+        });
+    };
+}(window.jQuery));
+
+$(function () {
+    $('.vertical .items').animateCarousel();
+});
