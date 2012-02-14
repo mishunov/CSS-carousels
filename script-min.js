42e1c426b95e3ecefe05098de28e9fab7bf1c33c
Denys Mishunov <denys.mishunov@gmail.com>
2012-02-14 10:52:12 +0100

    Parametrized colorDissolve

--- script-min.js
+++ script-min.js
@@ -99,7 +99,6 @@ $(function () {
         }
         var i = 0;
         next();
-
         function next() {
             if (callback.call(array[i], i, array[i]) !== false) {
                 if (++i < array.length) {
@@ -109,20 +108,29 @@ $(function () {
         }
         return array;
     };
-    $.fn.slowEach = function (interval, callback) {
-        return $.slowEach(this, interval, callback);
+    $.fn.colorDissolve = function (options) {
+        var op = $.extend(defaults, options),
+            interval = op.visibleItemDuration + 2 * op.transitionDuration;
+        return $.slowEach(this, interval, function () {
+            $(this)
+                .animate({
+                    opacity: 1
+                }, op.transitionDuration)
+                .delay(op.visibleItemDuration)
+                .animate({
+                    opacity: 0
+                }, op.transitionDuration);
+        });
+    };
+    var defaults = {
+        visibleItemDuration: 4000, // 4 seconds
+        transitionDuration: 1000 // 1 second
     };
 }(window.jQuery));
 
 $(function () {
-    $('.colorDissolve img').slowEach(7000, function () {
-        $(this)
-            .animate({
-                opacity: 1
-            }, 1000)
-            .delay(5000)
-            .animate({
-                opacity: 0
-            }, 1000);
+    $('.colorDissolve img').colorDissolve({
+        visibleItemDuration: 4000,
+        transitionDuration: 1000
     });
 });
